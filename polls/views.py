951b61e9bb98f0a50d71353e497e060a26f23c1e
Zachary Robbins <zrrobbins@wpi.edu>
2016-05-25 16:23:45 -0400

    Finished up through part 5 of tutorial. May write more tests and experiment with an in-browser framework and/or enable code coverage with coverage.py

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -8,55 +9,66 @@ from django.http import Http404
 from .models import Choice, Question
 
 class IndexView(generic.ListView):
-	template_name = 'polls.index.html'
-	context_object_name = 'latest_question_list'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-	def get_queryset(self):
-		# Return the last five published questions.
-		return Question.objects.order_by('-pub_date')[:5]
+    def get_queryset(self):
+        """
+        Return the last five pulished questions (not including those set to be 
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
-	model = Question
-	template_name = 'polls/detail.html'
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
-	model = Question
-	template_name = 'polls/results.html'
+    model = Question
+    template_name = 'polls/results.html'
 
-# Old way of doing it (without generics)	
+# Old way of doing it (without generics)    
 """
 def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = {
-		'latest_question_list': latest_question_list,
-	}
-	# return HttpResponse(template.render(context,request))
-	return render(request, 'polls/index.html', context)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # return HttpResponse(template.render(context,request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
-	# return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    # return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
-	"""
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    """
 
 def vote(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	try:
-		selected_choice = question.choice_set.get(pk=request.POST['choice'])
-	except(KeyError, Choice.DoesNotExist):
-		# Redisplay the question voting form
-		return render(request, 'polls/detail.html', {
-			'question': question,
-			'error_message': "You didn't select a choice.",
-		})
-	else:
-		selected_choice.votes += 1
-		selected_choice.save()
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
 
-		# Prevents accidental resubmittal from pressing the back button
-		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        # Prevents accidental resubmittal from pressing the back button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
